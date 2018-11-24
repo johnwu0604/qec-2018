be4e590505198bd901a6fa8c074c18d0a84eccc2
John Wu <john.wu@mail.mcgill.ca>
2018-11-24 14:26:54 -0500

    Got servos working

--- mec_2018/mec_2018.ino
+++ mec_2018/mec_2018.ino
@@ -1,25 +1,30 @@
 #include <Servo.h>
 
-const int EN_L = 11;
+const int EN_L = 8;
 const int EN_R = 3;
 
 // left motor
-const int INA_L = 10;
+const int INA_L = 11;
 const int INB_L = 9;
 
 // right motor
 const int INA_R = 6;
 const int INB_R = 5;
 
+// speed
+int currentSpeed = 255;
+
 Servo servos1; 
-Servo servos2;
-int SERVOS_1_PIN = 10;
+int SERVOS_PIN = 10;
 
 // Serial Variables
 int incomingByte = 0;   
 
 void setup() {
         Serial.begin(9600); 
+
+        servos1.attach(SERVOS_PIN);
+        servos1.write(90);
         
         pinMode(INA_L, OUTPUT);
         pinMode(INB_L, OUTPUT);
@@ -33,7 +38,6 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
-    Serial.println(incomingByte);
     if (incomingByte == 119) { // w
       forward();
     }
@@ -49,57 +53,103 @@ void loop() {
       turnRight();
     }
 
-    delay(80);
+    if (incomingByte == 112) { //p
+      increaseSpeed();
+    }
+
+    if (incomingByte == 108) { //l
+      decreaseSpeed();
+    }
+
+    if (incomingByte == 109) { //m
+      raiseArm();
+    }
+
+    if (incomingByte == 110) { //n
+      lowerArm();
+    }
+
+    delay(20);
     stopMotors();
   }
 }
 
 void forward() {
   digitalWrite(EN_L, HIGH);
-  digitalWrite(INA_L, HIGH);
-  digitalWrite(INB_L, LOW);
+  analogWrite(INA_L, 255);
+  analogWrite(INB_L, 0);
 
   digitalWrite(EN_R, HIGH);
-  digitalWrite(INA_R, HIGH);
-  digitalWrite(INB_R, LOW);
+  analogWrite(INA_R, currentSpeed);
+  analogWrite(INB_R, 0);
 }
 
 void backward() {
   digitalWrite(EN_L, HIGH);
-  digitalWrite(INA_L, LOW);
-  digitalWrite(INB_L, HIGH);
+  analogWrite(INA_L, 0);
+  analogWrite(INB_L, 255);
 
   digitalWrite(EN_R, HIGH);
-  digitalWrite(INA_R, LOW);
-  digitalWrite(INB_R, HIGH);
+  analogWrite(INA_R, 0);
+  analogWrite(INB_R, currentSpeed);
 }
 
 void turnLeft() {
   digitalWrite(EN_L, HIGH);
-  digitalWrite(INA_L, LOW);
-  digitalWrite(INB_L, HIGH);
+  analogWrite(INA_L, 0);
+  analogWrite(INB_L, 255);
 
   digitalWrite(EN_R, HIGH);
-  digitalWrite(INA_R, HIGH);
-  digitalWrite(INB_R, LOW);
+  analogWrite(INA_R, currentSpeed);
+  analogWrite(INB_R, 0);
 }
 
 void turnRight() {
   digitalWrite(EN_L, HIGH);
-  digitalWrite(INA_L, HIGH);
-  digitalWrite(INB_L, LOW);
+  analogWrite(INA_L, 255);
+  analogWrite(INB_L, 0);
 
   digitalWrite(EN_R, HIGH);
-  digitalWrite(INA_R, LOW);
-  digitalWrite(INB_R, HIGH);
+  analogWrite(INA_R, 0);
+  analogWrite(INB_R, currentSpeed);
 }
 
 void stopMotors() {
   digitalWrite(EN_L, LOW);
-  digitalWrite(INA_L, LOW);
-  digitalWrite(INB_L, LOW);
+  analogWrite(INA_L, 0);
+  analogWrite(INB_L, 0);
 
   digitalWrite(EN_R, LOW);
-  digitalWrite(INA_R, LOW);
-  digitalWrite(INB_R, LOW);
+  analogWrite(INA_R, 0);
+  analogWrite(INB_R, 0);
+}
+
+void lowerArm() {
+  Serial.print("Lower arm");
+  servos1.write(50);
+  delay(10);
+  servos1.write(90);
+}
+
+void raiseArm() {
+  Serial.print("Raise arm");
+  servos1.write(130);
+  delay(10);
+  servos1.write(90);
+}
+
+void increaseSpeed() {
+  if (currentSpeed < 255) {
+    currentSpeed+=5;
+  }
+  Serial.print("Current speed: ");
+  Serial.println(currentSpeed);
+}
+
+void decreaseSpeed() {
+  if (currentSpeed > 100) {
+    currentSpeed-=5;
+  }
+  Serial.print("Current speed: ");
+  Serial.println(currentSpeed);
 }
